c4731222500a6bf9af2618c30a6d2731bc3626d8
Silviya Thapa <silvithapa08@gmail.com>
2025-02-22 15:46:01 +0545

    second commit

--- main.c
+++ main.c
@@ -18,3 +18,37 @@ void cleanupSDL(SDL_Window *window, SDL_Renderer *renderer) {
     SDL_DestroyWindow(window);
     SDL_Quit();
 }
+void handleEvents(bool *running) {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        if (event.type == SDL_QUIT) {
+            *running = false;
+        }
+    }
+}
+
+Vehicle readVehicleFromFile(FILE *file) {
+    Vehicle vehicle = {0};
+    if (fscanf(file, "%f %f %d %d %d %d %d", 
+           &vehicle.x, &vehicle.y, 
+           (int*)&vehicle.direction, 
+           (int*)&vehicle.type, 
+           (int*)&vehicle.turnDirection, 
+           (int*)&vehicle.state, 
+           &vehicle.speed) == 7) {
+        vehicle.active = true;
+        
+        // Set dimensions based on direction
+        if (vehicle.direction == DIRECTION_NORTH || vehicle.direction == DIRECTION_SOUTH) {
+            vehicle.rect.w = 20;
+            vehicle.rect.h = 30;
+        } else {
+            vehicle.rect.w = 30;
+            vehicle.rect.h = 20;
+        }
+        
+        vehicle.rect.x = (int)vehicle.x;
+        vehicle.rect.y = (int)vehicle.y;
+    }
+    return vehicle;
+}
