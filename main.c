bd839204a538d42ec9c39639aaf3d572ddc63bdf
Silviya Thapa <silvithapa08@gmail.com>
2025-02-22 15:43:55 +0545

    1

--- main.c
+++ main.c
@@ -1,1 +1,20 @@
-//hello
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "traffic_simulation.h"
+#include<SDL.h>
+
+void initializeSDL(SDL_Window **window, SDL_Renderer **renderer) {
+    SDL_Init(SDL_INIT_VIDEO);
+    *window = SDL_CreateWindow("Traffic Simulation", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+    *renderer = SDL_CreateRenderer(*window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_SetRenderDrawColor(*renderer, 173, 216, 230, 255); // Light blue
+ 
+}
+
+
+void cleanupSDL(SDL_Window *window, SDL_Renderer *renderer) {
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
