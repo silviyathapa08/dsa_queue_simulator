da3a367cfb2c09cd142c9b3ac366398e530461b5
Silviya Thapa <silvithapa08@gmail.com>
2025-02-23 11:31:45 +0545

    Update generator.c

--- generator.c
+++ generator.c
@@ -2,3 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include "traffic_simulation.h"
+
+void writeVehicleToFile(FILE *file, Vehicle *vehicle) {
+    fprintf(file, "%f %f %d %d %d %d %d\n", 
+            vehicle->x, vehicle->y, 
+            vehicle->direction, 
+            vehicle->type, 
+            vehicle->turnDirection, 
+            vehicle->state, 
+            vehicle->speed);
+}
