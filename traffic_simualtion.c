a33b1da7810f6f89471406699a765ee472affe77
Silviya Thapa <silvithapa08@gmail.com>
2025-02-23 12:53:41 +0545

    Update traffic_simualtion.c

--- traffic_simualtion.c
+++ traffic_simualtion.c
@@ -13,3 +13,32 @@ const SDL_Color VEHICLE_COLORS[] = {
     {0, 0, 128, 255}, // POLICE_CAR: Dark Blue
     {255, 69, 0, 255} // FIRE_TRUCK: Orange-Red
 };
+//vehicle addition
+// renderVehicle(renderer, vehicleType, vehicleRect.x, vehicleRect.y, vehicleRect.w, vehicleRect.h);
+//tried keeping imaeges but was too frustating so goig with the rectangular blocks!! 
+
+
+void initializeTrafficLights(TrafficLight *lights)
+{
+    lights[0] = (TrafficLight){
+        .state = RED,
+        .timer = 0,
+        .position = {INTERSECTION_X - LANE_WIDTH, INTERSECTION_Y - LANE_WIDTH - TRAFFIC_LIGHT_HEIGHT, TRAFFIC_LIGHT_WIDTH, TRAFFIC_LIGHT_HEIGHT},
+        .direction = DIRECTION_NORTH};
+    lights[1] = (TrafficLight){
+        .state = RED,
+        .timer = 0,
+        .position = {INTERSECTION_X - LANE_WIDTH, INTERSECTION_Y + LANE_WIDTH, TRAFFIC_LIGHT_WIDTH, TRAFFIC_LIGHT_HEIGHT},
+        .direction = DIRECTION_SOUTH};
+    lights[2] = (TrafficLight){
+        .state = GREEN,
+        .timer = 0,
+        .position = {INTERSECTION_X + LANE_WIDTH, INTERSECTION_Y - LANE_WIDTH, TRAFFIC_LIGHT_HEIGHT, TRAFFIC_LIGHT_WIDTH},
+        .direction = DIRECTION_EAST};
+        //traffic light generation 
+    lights[3] = (TrafficLight){
+        .state = GREEN,
+        .timer = 0,
+        .position = {INTERSECTION_X - LANE_WIDTH - TRAFFIC_LIGHT_HEIGHT, INTERSECTION_Y - LANE_WIDTH, TRAFFIC_LIGHT_HEIGHT, TRAFFIC_LIGHT_WIDTH},
+        .direction = DIRECTION_WEST};
+}
