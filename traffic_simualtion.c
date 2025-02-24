712b98e177d2ed0a72309dfa64e0d8f7dd50bf8b
Silviya Thapa <silvithapa08@gmail.com>
2025-02-24 08:11:36 +0545

    Update traffic_simualtion.c

--- traffic_simualtion.c
+++ traffic_simualtion.c
@@ -102,3 +102,207 @@ Vehicle *createVehicle(Direction direction)
     {
         vehicle->type = REGULAR_CAR;
     }
+    vehicle->active = true;
+    // Set speed based on vehicle type
+    switch (vehicle->type)
+    {
+    case AMBULANCE:
+    case POLICE_CAR:
+        vehicle->speed = 4.0f;
+        break;
+    case FIRE_TRUCK:
+        vehicle->speed = 3.5f;
+        break;
+    default:
+        vehicle->speed = 2.0f;
+    }
+
+    vehicle->state = STATE_MOVING;
+    vehicle->turnAngle = 0.0f;
+    vehicle->turnProgress = 0.0f;
+
+    // 30% chance to turn
+    int turnChance = rand() % 100;
+    if (turnChance < 30)
+    {
+        vehicle->turnDirection = (turnChance < 15) ? TURN_LEFT : TURN_RIGHT;
+    }
+    else
+    {
+        vehicle->turnDirection = TURN_NONE;
+    }
+
+    // Set dimensions based on direction
+    if (direction == DIRECTION_NORTH || direction == DIRECTION_SOUTH)
+    {
+        vehicle->rect.w = 20; // width
+        vehicle->rect.h = 30; // height
+    }
+    else
+    {
+        vehicle->rect.w = 30; // width
+        vehicle->rect.h = 20; // height
+    }
+
+    // Fixed spawn positions for each direction
+    switch (direction)
+    {
+    case DIRECTION_NORTH:                             // Spawns at bottom, moves up
+        vehicle->x = INTERSECTION_X - LANE_WIDTH / 2; // Left lane
+        if (rand() % 2)
+        { // Randomly choose right lane
+            vehicle->x += LANE_WIDTH;
+        }
+        vehicle->y = WINDOW_HEIGHT - vehicle->rect.h;
+        vehicle->isInRightLane = (vehicle->x > INTERSECTION_X);
+        break;
+
+    case DIRECTION_SOUTH:                             // Spawns at top, moves down
+        vehicle->x = INTERSECTION_X - LANE_WIDTH / 2; // Left lane
+        if (rand() % 2)
+        { // Randomly choose right lane
+            vehicle->x += LANE_WIDTH;
+        }
+        vehicle->y = 0;
+        vehicle->isInRightLane = (vehicle->x > INTERSECTION_X);
+        break;
+
+    case DIRECTION_EAST: // Spawns at left, moves right
+        vehicle->x = 0;
+        vehicle->y = INTERSECTION_Y - LANE_WIDTH / 2; // Top lane
+        if (rand() % 2)
+        { // Randomly choose bottom lane
+            vehicle->y += LANE_WIDTH;
+        }
+        vehicle->isInRightLane = (vehicle->y > INTERSECTION_Y);
+        break;
+
+    case DIRECTION_WEST: // Spawns at right, moves left
+        vehicle->x = WINDOW_WIDTH - vehicle->rect.w;
+        vehicle->y = INTERSECTION_Y - LANE_WIDTH / 2; // Top lane
+        if (rand() % 2)
+        { // Randomly choose bottom lane
+            vehicle->y += LANE_WIDTH;
+        }
+        vehicle->isInRightLane = (vehicle->y > INTERSECTION_Y);
+        break;
+    }
+
+    // Center vehicle in lane
+    if (direction == DIRECTION_NORTH || direction == DIRECTION_SOUTH)
+    {
+        vehicle->x += (LANE_WIDTH / 4 - vehicle->rect.w / 2); // Center in lane
+    }
+    else
+    {
+        vehicle->y += (LANE_WIDTH / 4 - vehicle->rect.h / 2); // Center in lane
+    }
+
+    vehicle->rect.x = (int)vehicle->x;
+    vehicle->rect.y = (int)vehicle->y;
+
+    return vehicle;
+}
+void updateVehicle(Vehicle *vehicle, TrafficLight *lights)
+{
+    if (!vehicle->active)
+        return;
+
+    float stopLine = 0;
+    bool shouldStop = false;
+    float stopDistance = 40.0f;
+    float turnPoint = 0;
+    bool hasEmergencyPriority = (vehicle->type != REGULAR_CAR);
+
+    // Calculate stop line based on direction
+    switch (vehicle->direction)
+    {
+    case DIRECTION_NORTH:
+        stopLine = INTERSECTION_Y + LANE_WIDTH + 40;
+        if (vehicle->turnDirection == TURN_LEFT)
+        {
+            turnPoint = INTERSECTION_Y - LANE_WIDTH / 4;
+        }
+        else if (vehicle->turnDirection == TURN_RIGHT)
+        {
+            turnPoint = INTERSECTION_Y + LANE_WIDTH / 4;
+        }
+        else
+        {
+            turnPoint = INTERSECTION_Y;
+        }
+        break;
+    case DIRECTION_SOUTH:
+        stopLine = INTERSECTION_Y - LANE_WIDTH - 40;
+        if (vehicle->turnDirection == TURN_LEFT)
+        {
+            turnPoint = INTERSECTION_Y + LANE_WIDTH / 4;
+        }
+        else if (vehicle->turnDirection == TURN_RIGHT)
+        {
+            turnPoint = INTERSECTION_Y - LANE_WIDTH / 4;
+        }
+        else
+        {
+            turnPoint = INTERSECTION_Y;
+        }
+        break;
+    case DIRECTION_EAST:
+        stopLine = INTERSECTION_X - LANE_WIDTH - 40;
+        if (vehicle->turnDirection == TURN_LEFT)
+        {
+            turnPoint = INTERSECTION_X + LANE_WIDTH / 4;
+        }
+        else if (vehicle->turnDirection == TURN_RIGHT)
+        {
+            turnPoint = INTERSECTION_X - LANE_WIDTH / 4;
+        }
+        else
+        {
+            turnPoint = INTERSECTION_X;
+        }
+        break;
+    case DIRECTION_WEST:
+        stopLine = INTERSECTION_X + LANE_WIDTH + 40;
+        if (vehicle->turnDirection == TURN_LEFT)
+        {
+            turnPoint = INTERSECTION_X - LANE_WIDTH / 4;
+        }
+        else if (vehicle->turnDirection == TURN_RIGHT)
+        {
+            turnPoint = INTERSECTION_X + LANE_WIDTH / 4;
+        }
+        else
+        {
+            turnPoint = INTERSECTION_X;
+        }
+        break;
+    }
+
+    // Check if vehicle should stop based on traffic lights
+    if (!hasEmergencyPriority)
+    {
+        switch (vehicle->direction)
+        {
+        case DIRECTION_NORTH:
+            shouldStop = (vehicle->y > stopLine - stopDistance) &&
+                         (vehicle->y < stopLine) &&
+                         lights[DIRECTION_NORTH].state == RED;
+            break;
+        case DIRECTION_SOUTH:
+            shouldStop = (vehicle->y < stopLine + stopDistance) &&
+                         (vehicle->y > stopLine) &&
+                         lights[DIRECTION_SOUTH].state == RED;
+            break;
+        case DIRECTION_EAST:
+            shouldStop = (vehicle->x < stopLine + stopDistance) &&
+                         (vehicle->x > stopLine) &&
+                         lights[DIRECTION_EAST].state == RED;
+            break;
+        case DIRECTION_WEST:
+            shouldStop = (vehicle->x > stopLine - stopDistance) &&
+                         (vehicle->x < stopLine) &&
+                         lights[DIRECTION_WEST].state == RED;
+            break;
+        }
+    }
